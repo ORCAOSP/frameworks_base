e407bacd3a11ef4dd548032830beddefce699500
orcaosp <orcaproject@yahoo.com>
2013-05-13 17:58:28 -0400

    FW: Ribbon - Toggles
    Fix for some toggles not getting set properly
    Fix for ribbons in other ui modes, mostly refering to tablet ui mode.
    fix user toggles from force closing
    fix some toggles hieght being too large
    Icons from Nitroz
    
    Change-Id: Ia225e822485b715e6daddeb7b62e232e72db5e36

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/OrcaToolsToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/OrcaToolsToggle.java
@@ -11,7 +11,7 @@ import com.android.systemui.R;
 public class OrcaToolsToggle extends BaseToggle {
 
     @Override
-    protected void init(Context c, int style) {
+    public void init(Context c, int style) {
         super.init(c, style);
         setIcon(R.drawable.ic_qs_bf_settings);
         setLabel(R.string.quick_settings_orcatools);
