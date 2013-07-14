f3176a4ec8514f8323b05dac64d394400208b722
Orca Project <shamukillaz@orcaproject.co>
2013-07-14 16:29:09 -0400

    Fix Torch Toggle and LongPress

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
@@ -53,6 +53,7 @@ import android.widget.FrameLayout;
 
 import com.android.internal.R;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.util.cm.TorchConstants;
 
 /**
  * Manages creating, showing, hiding and resetting the keyguard.  Calls back
@@ -308,7 +309,7 @@ public class KeyguardViewManager {
                     break;
                 case KeyEvent.KEYCODE_CAMERA:
                     action = Settings.System.LOCKSCREEN_LONG_CAMERA_ACTION;
-                    break;                    
+                    break;
             }
 
             if (action != null) {
@@ -354,7 +355,7 @@ public class KeyguardViewManager {
 
     private static boolean runAction(Context context, String uri) {
         if ("FLASHLIGHT".equals(uri)) {
-            context.sendBroadcast(new Intent("net.cactii.flash2.TOGGLE_FLASHLIGHT"));
+            context.sendBroadcast(new Intent(TorchConstants.ACTION_TOGGLE_STATE));
             return true;
         } else if ("NEXT".equals(uri)) {
             sendMediaButtonEvent(context, KeyEvent.KEYCODE_MEDIA_NEXT);
